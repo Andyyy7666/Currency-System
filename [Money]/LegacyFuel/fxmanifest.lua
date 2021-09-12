88b0ec196414d403d174e5e1e8f0450c866c5c55
HypnoticSiege <65150090+HypnoticSiege@users.noreply.github.com>
2021-09-12 00:22:14 -0400

    Added LegacyFuel Integration

---
+++ [Money]/LegacyFuel/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'InZidiuZ'
+description 'Legacy Fuel'
+version '1.3'
+
+-- What to run
+client_scripts {
+	'config.lua',
+	'functions/functions_client.lua',
+	'source/fuel_client.lua'
+}
+
+server_scripts {
+	'config.lua',
+	'source/fuel_server.lua'
+}
+
+exports {
+	'GetFuel',
+	'SetFuel'
+}
