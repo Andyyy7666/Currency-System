70ed71d06303fc2003d826f75e9c8be489cd19ab
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-07-13 19:45:19 +0200

    2.0.0
    * Move client config to server since there was no point in having it in the client when I can just use it on the server and remove any of that code from the client.
    * Added permissions to admin commands.

--- [Money]/Money_Script/config_server.lua
+++ [Money]/Money_Script/config_server.lua
@@ -1,16 +1,24 @@
-------------------------------------------------------------------------
---                                                                    --
---	   For support join my discord: https://discord.gg/Z9Mxu72zZ6	  --
---                                                                    --
-------------------------------------------------------------------------
+-- For support join my discord: https://discord.gg/Z9Mxu72zZ6
 
-discord_config = {
-    discordToken = "", -- Create a bot and write the token in here.
-    guildId = "857672921912836116", -- The discord server id.
-    roles = { -- Roles that can use the adminAddCommand.
-        ["ADMIN"] = 857703285552054272,
-    },
+server_config = {
+    startingCash = 2500, -- Amount of starting money in wallet.
+    startingBank = 8000, -- Amount of starting money in the bank.
 
-    enableDiscordLogs = true, -- Logs for the admin command (requires a webhook).
+    salaryAmount = 300, -- the daily amount that players will receive.
+    salaryInterval = 24, -- every x minutes the player will receive the salaryAmount.
+
+    payCommand = "pay", -- This command will transfer a player money from your bank account.
+    giveCommand = "give", -- This command will give a player close to you money from your wallet.
+
+    enableAddCommand = false, -- if you want to enable the command below turn this to true.
+    adminAddCommand = "addmoney", -- This command is used to add money to players. I would suggest you set up permissions for only admins below.
+    discordServerToken = "", -- This is your discord bot token.
+    guildId = "", -- Discord server id.
+    enableDiscordLogs = true, -- If you want to log the addmoney command to a discord channel then turn this to true and add a webhook below.
     discordWebhook = "",
+
+    -- Add the role id's below for the roles you want addmoney command to use.
+    roles = {
+        ""
+    }
 }
